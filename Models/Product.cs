cb4e93d861df689222573a02ebf85fe0c045b4a0
Noel Lopes <noel@ipg.pt>
2023-10-13 11:47:21 +0100

    Add product model

---
+++ Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SportsStore.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        public required string Name { get; set; }
+
+        public string? Description { get; set; }
+
+        [StringLength(50)]
+        public string? Category { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
