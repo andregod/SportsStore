b2cd9e207902e74c6587092a2ec4791e84880ce7
Noel Lopes <noel@ipg.pt>
2023-10-13 12:06:07 +0100

    Add a fake repository

---
+++ Data/FakeRepository.cs
@@ -0,0 +1,27 @@
+﻿using SportsStore.Models;
+
+namespace SportsStore.Data
+{
+    public class FakeRepository : ISportsStoreRepository
+    {
+        public IEnumerable<Product> Products =>
+            new List<Product>
+            {
+                new Product
+                {
+                    Name = "Football ball",
+                    Price = 25
+                },
+                new Product
+                {
+                    Name = "Surfboard",
+                    Price = 375
+                },
+                new Product
+                {
+                    Name = "Running shoes",
+                    Price = 95
+                }
+            };
+    }
+}
