dc2106e4120cccafe15b149957c70980bbd96837
andregod <andre_godinho1@hotmail.com>
2023-10-18 15:15:46 +0100

    Create a database repository to replace the fake one

---
+++ Data/SportsStoreDatabaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using SportsStore.Models;
+
+namespace SportsStore.Data
+{
+    public class SportsStoreDatabaseRepository: ISportsStoreRepository
+    {
+        private SportsStoreDbContext _context;
+        public SportsStoreDatabaseRepository(SportsStoreDbContext context) {
+            _context = context;
+        }
+
+        public IEnumerable<Product> Products => _context.Products;
+    }
+}
--- Data/SportsStoreDbContext.cs
+++ Data/SportsStoreDbContext.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
 
 namespace SportsStore.Data
 {
     public class SportsStoreDbContext : DbContext
     {
+        public SportsStoreDbContext(DbContextOptions<SportsStoreDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products {get;set; }
     }
 }
